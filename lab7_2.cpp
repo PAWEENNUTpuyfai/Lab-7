1304fcc151f7b911dcfebd4debbf38a769fc96bb
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2022-12-13 16:02:31 +0700

    Lap7_3.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,12 +3,27 @@
 using namespace std;
 
 int main(){
-	
 	int i = 0, P[5000], k, result;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout << "Please input k: ";
+	cin >> k;
+	if (k <= 0){
+		cout << "Invalid input!!!";
+	}else{
+	    i=0;
+	    result=0;
+	    while (i<5000){
+		    if (P[i]%k==0){
+			    result -=P[i];
+		    }else{
+			    result +=P[i];
+		    }
+		    i++;
+	    }
+	    cout << "Result = " << result;
+	}
 	return 0;
 }
